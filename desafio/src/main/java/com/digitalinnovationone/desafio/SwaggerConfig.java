c2246247fcb5a41c34cfe72306a054f8f885eda3
GabrielSilva2y3d <74156876+GabrielSilva2y3d@users.noreply.github.com>
2021-08-04 17:38:23 -0300

    swagger 3

--- desafio/src/main/java/com/digitalinnovationone/desafio/SwaggerConfig.java
+++ desafio/src/main/java/com/digitalinnovationone/desafio/SwaggerConfig.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafioApplication {
+public class SwaggerConfig {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DesafioApplication.class, args);
+		SpringApplication.run(SwaggerConfig.class, args);
 	}
 
 }
